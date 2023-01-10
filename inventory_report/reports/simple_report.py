059ea96db3016f83255d25c39c6b8ad36780b9f1
FabioMartins <fabbio1000@gmail.com>
2023-01-09 23:43:33 -0300

    Fix: Corrigindo erro de formatação

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,19 +1,20 @@
-from datetime import datetime
 from collections import Counter
+
 
 class SimpleReport:
     @staticmethod
-    def generate(list_of_products):
+    def generate(products):
 
-        fabricacao_mais_antiga = [item["data_de_fabricacao"] for item in list_of_products]
+        mais_antiga = [item["data_de_fabricacao"] for item in products]
 
-        validade_mais_proxima = [item["data_de_validade"] for item in list_of_products]
+        validade_proxima = [item["data_de_validade"] for item in products]
 
-        empresa_mais_produtos = Counter([item["nome_da_empresa"] for item in list_of_products]).most_common(1)[0][0]
-
+        empresa_mais_produtos = Counter(
+            [item["nome_da_empresa"] for item in products]
+            ).most_common(1)[0][0]
 
         return (
-            f"Data de fabricação mais antiga: {min(fabricacao_mais_antiga)}\n"
-            f"Data de validade mais próxima: {min(validade_mais_proxima)}\n"
+            f"Data de fabricação mais antiga: {min(mais_antiga)}\n"
+            f"Data de validade mais próxima: {min(validade_proxima)}\n"
             f"Empresa com mais produtos: {empresa_mais_produtos}"
         )
