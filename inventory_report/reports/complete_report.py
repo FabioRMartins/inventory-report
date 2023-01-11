27616e9c8140ff38d6c433fea7708eb3753fc516
FabioMartins <fabbio1000@gmail.com>
2023-01-11 16:14:33 -0300

    Feat: Gerando a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(file):    
+
+        report = SimpleReport.generate(file)
+
+        empresa = [item["nome_da_empresa"] for item in file]
+        contagem_estoque = Counter(empresa)
+
+        report += "\nProdutos estocados por empresa:\n"
+        for nome_empresa, quantidade in contagem_estoque.items():
+            report += f" - {nome_empresa}: {quantidade}\n"
+
+        return report
